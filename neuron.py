57e9c74391da73260a06f55a08f6534b92902c39
mattragoza <mtr22@pitt.edu>
2016-04-19 18:42:01 -0400

    refactor Neuron to handle negative weights, some doc and error checking

--- neuron.py
+++ neuron.py
@@ -7,26 +7,39 @@ class Neuron:
     def __init__(self, base_rate, max_rate, rec_field, alpha=0.5, init_weights=None):
 
         self.base_rate = base_rate
-        self.firing_rate = base_rate
-        self.rec_field = rec_field
-        self.ouput = [] #other neurons this neuron connects TO
-        self.inputs = [] #neurons that connect to this neuron
-        self.alpha = alpha
         self.max_rate = max_rate
-        #TODO: make check that init_weights is same dimension as rec_field
-        self.weights = init_weights
+        self.firing_rate = base_rate
+        self.alpha = alpha
+        
+        self.ouput  = [] #other neurons this neuron connects TO
+        self.inputs = [] #neurons that connect to this neuron
+
+        self.rec_field = rec_field
         if init_weights is None:
-            self.weights = np.random.random((rec_field, rec_field))
+            self.weights = np.random.normal(0, 1, (rec_field, rec_field))
+        elif init_weights.shape == (rec_field, rec_field):
+            self.weights = init_weights
+        else:
+            raise ValueError("init_weights shape must match rec_field")
             
     def update_firing_rate(self, stim):
 
+        # take weighted sum of input stimulus
         input_sum = np.dot(self.weights.reshape(-1), stim.reshape(-1))
-        new_rate = sigmoid(input_sum)*(self.max_rate - self.base_rate) + self.base_rate
-        self.firing_rate = (1-self.alpha)*new_rate + self.alpha*(self.firing_rate)
+
+        # map the input sum into the firing rate range
+        new_rate = sigmoid(input_sum, y_mid=self.base_rate, \
+            y_range=2*(self.max_rate-self.base_rate))
+
+        # update firing rate, taking into account the previous
+        # rate weighted by the alpha parameter
+        self.firing_rate = (1-self.alpha)*new_rate + self.alpha*self.firing_rate
+
+        return self.firing_rate
 
 
-def sigmoid(x):
-  return 2*(1 / (1 + math.exp(-x)) - 0.5)
+def sigmoid(x, y_mid=0.5, y_range=1.0):
+  return y_range*(1 / (1 + math.exp(-x)) - 0.5 + y_mid)
 
 
 if __name__ == '__main__':
